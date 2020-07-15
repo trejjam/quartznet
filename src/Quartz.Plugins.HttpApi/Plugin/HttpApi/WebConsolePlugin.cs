681b4c2102809ca1068851b4489830ebffc95fc3
Marko Lahma <marko.lahma@gmail.com>
2020-07-15 09:27:23 +0300

    initial models

---
+++ src/Quartz.Plugins.HttpApi/Plugin/HttpApi/WebConsolePlugin.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+using Quartz.Spi;
+
+namespace Quartz.Plugin.HttpApi
+{
+    public class HttpApiPlugin : ISchedulerPlugin
+    {
+        private IHost? host;
+
+        public string HostName { get; set; } = "127.0.0.1";
+        public int? Port { get; set; }
+
+        public Task Initialize(string pluginName, IScheduler scheduler, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Start(CancellationToken cancellationToken)
+        {
+            string baseAddress = $"http://{HostName ?? "localhost"}:{Port ?? 28682}/";
+
+            //host = WebApp.Start<Startup>(url: baseAddress);
+            host = Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .Build();
+            
+            // log.InfoFormat("Quartz Web Console bound to address {0}", baseAddress);
+            return Task.CompletedTask;
+        }
+
+        public Task Shutdown(CancellationToken cancellationToken)
+        {
+            host?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
